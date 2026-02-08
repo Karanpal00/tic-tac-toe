a96a161ea059cab618a79c8778f9d53ad7b8944a
Karanpal00 <karan.pal.2204@gmail.com>
2026-02-08 19:25:18 +0530

    Add custom fonts and update styles. Add winning message display and reset functionality. Remove unnecessary console logs and comments. Remove text selection on the game board for better user experience. Remove the play button and start the game immediately on page load. Add reset button to clear the game board and start a new game.

--- script.js
+++ script.js
@@ -35,11 +35,6 @@ function GameBoard(size = 3) {
 
     const getBoard = () => board;
 
-    const printBoard = () => {
-        const boardValues = board.map((row) => row.map((cell) => cell.getValue()));
-        console.table(boardValues);
-    }
-
     const markToken = (row, col, player) => {
         if (board[row][col].getValue() !== null) {
             return false;
@@ -52,13 +47,15 @@ function GameBoard(size = 3) {
     return {
         initializeBoard,
         getBoard,
-        printBoard,
         markToken,
     }
 }
 
 // GameController function factory
 function GameController(playerOneName="Player1", playerTwoName="player2") {
+
+    const boardSize = 3;
+
     const players = [{
         name:playerOneName,
         token:'X'
@@ -68,7 +65,7 @@ function GameController(playerOneName="Player1", playerTwoName="player2") {
         token:'O'
     }];
 
-    const board = GameBoard();
+    const board = GameBoard(boardSize);
     
     let currentPlayer = players[0];
 
@@ -79,8 +76,9 @@ function GameController(playerOneName="Player1", playerTwoName="player2") {
 
     // get board values for the ui
     const getBoardValues = () => {
-        board.getBoard().map(row => row.map(cell => cell.getValue()));
+        return board.getBoard().map(row => row.map(cell => cell.getValue())).flat();
     }
+
     // check valid cells in the board
     const isValidCell = (row, col, boardSize) => {
         if (row >= 0 && row < boardSize && col >= 0 && col < boardSize) {
@@ -145,28 +143,21 @@ function GameController(playerOneName="Player1", playerTwoName="player2") {
         return false;
     }
 
-    // log the players turn
-    const log = () => {
-        console.log(`Now ${currentPlayer.name}'s turn.`);
-    }
-
     //play the game round 
-    const playRound = (row, col) => {
+    const playMove = (row, col) => {
         if (!board.markToken(row, col, currentPlayer.token)) {
-            console.log('Cannot play this move, try another one.');
-            return;
+            return null;
         }
 
         if (checkWin(row, col)) {
-            board.printBoard();
             return {winner : currentPlayer};
         } else if (isBoardFull()) {
             return {draw: true};
         }
 
         switchPlayer();
-        board.printBoard(); 
-        log(); 
+
+        return null;
     }
 
     //reset the game
@@ -175,14 +166,83 @@ function GameController(playerOneName="Player1", playerTwoName="player2") {
         currentPlayer = players[0];
     }
 
-    board.printBoard();
-    log();
-
     return {
-        playRound,
+        boardSize,
+        playMove,
         reset,
-        movesAvailable
+        movesAvailable,
+        getBoardValues,
     };
 }
 
-const game = GameController();
+// Dom controller function factory
+function DomController () {
+    const gameContainer = document.querySelector('#game-board');
+    const domGameBoard = document.querySelectorAll('.cell');
+    const winBoard = document.querySelector('#winBoard');
+    const restartBtn = document.querySelector('#restart-btn');
+
+    const game = GameController();
+
+    let isGameOver = null;
+
+    const gameOver = () => {
+        if (isGameOver === null) {
+            return;
+        }   
+
+        if ('winner' in isGameOver) {
+            winBoard.textContent = `${isGameOver.winner.name}  Won!`;
+
+        } else {
+            winBoard.textContent = `It's a Draw Fuck both of you!`;
+        }
+
+        gameContainer.style.display = 'none';
+        winBoard.style.display = 'block';
+    }
+
+    const uiReset = () => {
+        game.reset();
+        gameContainer.style.display = 'grid';
+        winBoard.style.display = 'none';
+        printBoard();        
+    }
+
+     const printBoard = () => {
+        const gridValues = game.getBoardValues();
+        for(let i = 0; i < domGameBoard.length; ++i) {
+            domGameBoard[i].textContent = gridValues[i];
+        }
+    }
+
+    const dropToken = (e) => {
+        const index1D = e.target.dataset.index;
+        const row = Math.floor(index1D/game.boardSize);
+        const col = index1D%game.boardSize;
+
+        isGameOver = game.playMove(row, col);
+        gameOver();
+        printBoard();
+    }   
+  
+    const addEvents = () => {
+        gameContainer.addEventListener('click', dropToken);
+        restartBtn.addEventListener('click', uiReset);
+    }    
+
+    addEvents();
+    printBoard();
+}   
+
+const dom = DomController();
+
+/* 
+Todo :-
+
+1.Add win screen.   [Done]
+2.Start with the modal taking player names or play against bot. 
+3.Add functionality to restart the game.
+    a.Once the restart button is clicked then the game should go to the modal.
+4.Add tic-tac-toe bot.
+*/
